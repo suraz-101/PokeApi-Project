c3560741342126dce77a359579b9c6e7ad65727a
Suraj Pandey <surazpandey101@gmail.com>
2024-03-17 22:11:53 +0545

    implementing useContext

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,22 +1,50 @@
-import { useState } from "react";
+import { useContext, useState } from "react"; // importing useContext to use the context
+import { createContext } from "react"; // importing createContext function to create the context
+
+const UserContext = createContext(null); // creating the contextvariable with default value null
 
 export const Context = () => {
-  const [name, setName] = useState("Suraj Pandey");
+  const [name, setName] = useState("Suraj ");
   return (
     <>
-      <Context1 name={name} />
+      <UserContext.Provider value={{ name, setName }}>
+        {/*context provider */}
+        {/* <Context1 /> */}
+        <Context1 />
+      </UserContext.Provider>
+      {/* <Context1 /> */}
     </>
   );
 };
 
-const Context1 = ({ name }) => {
+const Context1 = () => {
   return (
     <>
-      <Context2 name={name} />
+      <Context2 />
     </>
   );
 };
 
-const Context2 = ({ name }) => {
-  return <>Hellow : {name}</>;
+const Context2 = () => {
+  const username = useContext(UserContext); // consumer
+
+  return (
+    <>
+      Hellow :{username.name}
+      <button
+        onClick={() => {
+          username.setName("Suraj Pandey");
+        }}
+      >
+        Change Name
+      </button>
+      <button
+        onClick={() => {
+          username.setName("Suraj");
+        }}
+      >
+        Reset
+      </button>
+    </>
+  );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,6 +30,8 @@ export const Home = () => {
     return () => controller.abort();
   }, [pagination?.page, pagination?.limit]);
 
+  
+
   return (
     <>
       <div>
