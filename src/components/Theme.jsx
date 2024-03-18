0c1057401caae62a321bf2c54ae0f554689e4744
Suraj Pandey <surazpandey101@gmail.com>
2024-03-18 21:47:11 +0545

    working on theme

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,19 +1,28 @@
-// import { useEffect } from "react";
+import { useState } from "react";
 import { useContext, useEffect } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 export const ChangeTheme = () => {
+  // const [mode, setMode] = useState("light");
   const { theme, ToggleTheme } = useContext(ThemeContext);
 
   useEffect(() => {
     document.body.className = theme;
+    // document.getElementById("division").className = mode;
   }, [theme]);
 
   return (
     <>
-      <br />
-      Theme : {theme}
-      <button onClick={ToggleTheme}>Change Theme</button>
+      <div id="division" className="ligth">
+        <br />
+        Theme : {theme}
+        {/* <button
+          onClick={() => {
+            mode === "light" ? setMode("dark") : setMode("light");
+          }}
+        ></button> */}
+        <button onClick={ToggleTheme}>Change Theme</button>
+      </div>
     </>
   );
 };
